80971e9e7b1224d1cae10ccf1522a689feeb9a4c
Robert Smith <smith.rob@student.greenriver.edu>
2025-06-21 09:27:58 -0700

    Starter setup begining

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+const { app, BrowserWindow } = require("electron/main");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadFile("./components/index/index.html");
+};
+
+app.whenReady().then(() => {
+  console.log("App is ready");
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  console.log("All windows closed");
+  if (process.platform !== "darwin") 
+    app.quit();
+});
